37b2ac555abafe881f3f9a16c18877314b5ae531
ntsh5446 <nikandel1111@gmail.com>
2024-05-20 17:10:05 +0545

    initail commit

---
+++ client/src/app/page.js
@@ -0,0 +1,13 @@
+import NavBar from '@/components/navbar/NavBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <NavBar/>
+      this is home page
+    </div>
+  )
+}
+ 
+export default page
---
+++ client/src/components/navbar/NavBar.jsx
@@ -0,0 +1,84 @@
+ 'use client'
+import React from "react";
+import { useState } from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+
+
+const NavBar=()=> {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar onMenuOpenChange={setIsMenuOpen} className=" bg-green-800 p-4">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          {/* <AcmeLogo /> */}
+          <p className="font-bold text-inherit">ACME</p>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+        <Button as={Link}  href="#" variant="flat" className="hover:bg-green-900">
+            Login
+          </Button>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link}  href="#" variant="flat"className="hover:bg-green-900">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                index === 2 ? "primary" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
+export default NavBar
